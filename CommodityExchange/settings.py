875e54a47ad03bd92a23a1ded3442a46f739f6bd
chintan-27 <chintan.acharya27@gmail.com>
2023-09-12 22:27:32 -0400

    Updates from Readme

--- CommodityExchange/settings.py
+++ CommodityExchange/settings.py
@@ -43,7 +43,14 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'djdev_panel.middleware.DebugMiddleware',
+    # 'djdev_panel.middleware.DebugMiddleware',
+    # 'django.middleware.security.SecurityMiddleware',
+    # 'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'django.middleware.common.CommonMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.contrib.messages.middleware.MessageMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
